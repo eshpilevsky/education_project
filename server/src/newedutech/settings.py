03b430199839d59e2d54e3b8a23f0ed111418aac
Evgeny Shpilevsky <eshpilevsky@icloud.com>
2020-06-20 17:03:27 +0300

    Changed db postgres, added settings for deploy.

--- server/src/newedutech/settings.py
+++ server/src/newedutech/settings.py
@@ -92,8 +92,12 @@ EMAIL_USE_SSL = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newedutech',
+        'USER': 'newedutechuser',
+        'PASSWORD': 'newedutechpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
